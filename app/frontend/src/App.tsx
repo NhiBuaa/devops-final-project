c7e53f44cf80bc3c3dd13c6225b31facee9713ff
NhiBuaa <nhihuynh210905@gmail.com>
2026-05-14 08:36:53 +0700

    test (#100)

--- app/frontend/src/App.tsx
+++ app/frontend/src/App.tsx
@@ -80,7 +80,7 @@ function App() {
           </div>
           <div>
             <h1 className="text-3xl font-bold text-slate-800 tracking-tight">
-              Team Management 1234-
+              Team Management
             </h1>
             <p className="text-slate-500 mt-1">
               Manage your development team efficiently
